ae4d6dbfd4c41f154c11315cbb5cd443e3bd37a1
Yosri Swidan <yosrisaed99@gmail.com>
2025-03-03 13:03:24 -0700

    updated projects

--- src/data/CertificatesDataObject.js
+++ src/data/CertificatesDataObject.js
@@ -1,6 +1,7 @@
 import advanceReactCer from '../images/advanceReactCer.PNG'
 import tryHackMeCer from '../images/tryHackMeCer.PNG'
 import frontCarer from '../images/frontCarer.PNG'
+import frontCareer2 from '../images/frontCareer2.png'
 const data = [
     {
         id:1,
@@ -22,6 +23,13 @@ const data = [
         title:'The Frontend Career Path',
         url:'https://scrimba.com/certificate/uNbKDvS5/gfrontend',
         date:'2023/8/7'
+    },
+        {
+        id:4,
+        img:frontCareer2,
+        title:'The Frontend Developer Career Path',
+        url:'https://scrimba.com/certificate-cert2uNjfK9m1kjZ8rY7cHYBTKLUHYcSxEUegPi',
+        date:'2024/10/11'
     }
 ]
 export default data
--- src/data/ProjectsDataObject.js
+++ src/data/ProjectsDataObject.js
@@ -4,6 +4,16 @@ import tenzies from '../images/tenzies.PNG'
 import fitMeUp from '../images/fitMeUp.PNG'
 import portfilo from '../images/portfilo.PNG'
 import catchMe from '../images/catchMe.PNG'
+import ragingSea from '../images/ragingSea.png'
+import galaxy from '../images/galaxy.png'
+import smoke from '../images/smoke.png'
+import earth from '../images/earth.png'
+import liquid from '../images/liquid.png'
+import gpgpu from '../images/gpgpu.png'
+import wobble from '../images/wobble.png'
+import terrain from '../images/terrain.png'
+import portal from '../images/portal.png'
+import laptop from '../images/laptop.png'
 
 const data = [
     {
@@ -12,7 +22,7 @@ const data = [
         title:'Personal Website (old)',
         description:'An old personal website',
         url:'https://imyosrii.github.io/',
-        date:'2022/6/1'
+        date:'2022/Jun/1'
     },
     {
         id:2,
@@ -20,7 +30,7 @@ const data = [
         title:'Tasks manager chrome extention',
         description:'extention that helps manage your time and tasks',
         url:'https://github.com/ImYosrii/Tasks-Manager-extension-',
-        date:'2022/6/20'
+        date:'2022/Jun/20'
     },
     {
         id:3,
@@ -28,7 +38,7 @@ const data = [
         title:'Tenzies',
         description:'A game where you math dice by hold and rerolling until they all match',
         url:'https://github.com/ImYosrii/Tenzies',
-        date:'2022/7/19'
+        date:'2022/Jul/19'
     },
     {
         id:4,
@@ -36,7 +46,7 @@ const data = [
         title:'Fit Me Up',
         description:'An exercising website that offers 1300+ exercises with offering youtube videos for each exercise',
         url:'https://fit-me-up.netlify.app/',
-        date:'2022/8/28'
+        date:'2022/Aug/28'
     },
     {
         id:5,
@@ -44,7 +54,7 @@ const data = [
         title:'Personal Website',
         description:'A new personal website',
         url:'https://github.com/ImYosrii/portfolio',
-        date:'2022/8/30'
+        date:'2022/Aug/30'
     },
     {
         id:6,
@@ -52,7 +62,87 @@ const data = [
         title:'Catch Me if you Scan',
         description:'An exciting game that allows you to compete with your friends to become the ultimate monster collector.',
         url:'https://github.com/CMPUT301W23T07/catch-me-if-you-scan',
-        date:'2023/3/3'
+        date:'2023/Mar/3'
+    },
+    {
+        id:7,
+        img:ragingSea,
+        title:'Raging Sea GLSL Shader',
+        description:'A raging sea shader made with GLSL shaders and ThreeJS, with Lil-gui controls panel.',
+        url:'https://sea-waves-3d.netlify.app/',
+        date:'2025/Jan/4'
+    },
+    {
+        id:8,
+        img:galaxy,
+        title:'Galaxy Generator GLSL Shader',
+        description:'A galaxy generator shader made with GLSL shaders and ThreeJS, with Lil-gui controls panel.',
+        url:'https://galaxy-generator-3d.netlify.app/',
+        date:'2025/Jan/5'
+    },
+    {
+        id:9,
+        img:smoke,
+        title:'Coffee Smoke GLSL Shader',
+        description:'A coffee smoke shader made with GLSL shaders, Simplex Noise and ThreeJS.',
+        url:'https://smoke-shader.netlify.app/',
+        date:'2025/Jan/9'
+    },
+    {
+        id:10,
+        img:earth,
+        title:'A Rotating Earth with Sun GLSL Shader',
+        description:'A rotating earth with sun shader made with GLSL shaders and ThreeJS.',
+        url:'https://earth-shader.netlify.app/',
+        date:'2025/Jan/13'
+    },
+    {
+        id:11,
+        img:liquid,
+        title:'A Raging Sea but with custom Lights GLSL Shader',
+        description:'A raging sea but with custom lights shader made with GLSL shaders and ThreeJS.',
+        url:'https://sea-shader.netlify.app/',
+        date:'2025/Jan/13'
+    },
+    {
+        id:12,
+        img:gpgpu,
+        title:'A GLSL shader that follows GPGPU principles to simulate particles flow movement calculations using the GPU',
+        description:'shader made with GLSL shaders and ThreeJS that turns any model into a moving particles.',
+        url:'https://gpgpu-flow.netlify.app/',
+        date:'2025/Jan/21'
+    },
+    {
+        id:13,
+        img:wobble,
+        title:'A GLSL shader that simulates wobble movement on models',
+        description:'shader made with GLSL shaders and ThreeJS that simulates random wobble movements.',
+        url:'https://wobble-shader.netlify.app/',
+        date:'2025/Jan/22'
+    },
+    {
+        id:14,
+        img:terrain,
+        title:'A GLSL shader that simulates random Terrains',
+        description:'shader made with GLSL shaders and ThreeJS that simulates random terrain using Simplex Noise.',
+        url:'https://terrain-shader.netlify.app/',
+        date:'2025/Jan/24'
+    },
+    {
+        id:15,
+        img:portal,
+        title:'Portal scene with custom portal GLSL Shaders.',
+        description:'A Portal model that was created and baked its textures in Blender and rendered with ThreeJS with custom GLSL shaders for the portal effect and then rendered in React-Three-Fiber (R3F).',
+        url:'https://terrain-shader.netlify.app/',
+        date:'2025/Feb/17'
+    },
+    {
+        id:16,
+        img:laptop,
+        title:'A 3D Portfolio.',
+        description:'A laptop model that shows my original portfolio using iFrame tag. Website was created using R3F and Drei',
+        url:'https://yosri-swidan-3d.netlify.app/',
+        date:'2025/Mar/2'
     }
 ]
 export default data
